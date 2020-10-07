22d251d6e80111650ce04af00ed7bda79b11688f
natawik.spb@gamil.com <natawik.spb@gamil.com>
2020-10-07 14:44:39 +0300

    add lombok lib

--- src/main/java/bootcamp/bankApi/models/Account.java
+++ src/main/java/bootcamp/bankApi/models/Account.java
@@ -1,7 +1,13 @@
 package bootcamp.bankApi.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "accounts")
 public class Account {
@@ -10,48 +16,16 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NonNull
     @Column(name = "account_number")
     private String accountNumber;
 
+    @NonNull
     @Column(name = "balance")
     private float balance;
 
+    @NonNull
     @Column(name = "customer_id")
     private int customerId;
 
-    public Account() {}
-
-    public Account(String accountNumber, float balance, int customerId) {
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.customerId = customerId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public float getBalance() {
-        return balance;
-    }
-
-    public void setBalance(float balance) {
-        this.balance = balance;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
 }
